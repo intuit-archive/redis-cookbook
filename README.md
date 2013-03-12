f99a944d5e0e124e65baf140bf0ccdc417955057
Bradly Feeley <bradlyf@gmail.com>
2013-03-12 14:11:45 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# redis cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Intuit, Inc. (<bradly_feeley@intuit.com>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "redis"
+maintainer       "Intuit, Inc."
+maintainer_email "bradly_feeley@intuit.com"
+license          "Apache 2.0"
+description      "Installs/Configures redis"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
